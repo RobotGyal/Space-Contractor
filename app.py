58f1bc627e5265e88b15b1a8c6853d9a18635f49
Robotgyal <aleiaknight@gmail.com>
2019-10-07 20:49:44 -0700

    Added Store display route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import os 
 from dotenv import load_dotenv
 from pymongo import MongoClient
@@ -15,6 +15,19 @@ space_items = db.space_items    #sets up space items as a database
 def test():
     '''Main page setup function'''
     return render_template('store_index.html', space_items=space_items.find())
+
+# Route for VIEWING store items
+@app.route('/store/display')
+def store_display():
+    return render_template('store_display.html', space_items=space_items)
+
+
+#@app.route('store/cart', 'GET')
+
+
+
+#@app.route('store/cart', 'POST')
+
 
 
 if __name__=='__main__':
