cc3332f842aabc9e163f1f272b87d13584fe122d
Robotgyal <aleiaknight@gmail.com>
2019-10-07 20:35:11 -0700

    Setup mongodb. Added CSS to homepage

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, render_template
 import os 
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 #framework initializing 
 app = Flask(__name__)
 load_dotenv()
+client = MongoClient('localhost', 27017)
+db = client.space
+space_items = db.space_items    #sets up space items as a database
 
-#Main route
+# Main route
 @app.route('/')
 def test():
     '''Main page setup function'''
-    return "The page works"
+    return render_template('store_index.html', space_items=space_items.find())
 
 
 if __name__=='__main__':
