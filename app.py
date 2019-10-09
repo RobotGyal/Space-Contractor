70ab3ba09f6aeaea537eec4ed9d6a5ec7fed01e7
Robotgyal <aleiaknight@gmail.com>
2019-10-09 16:10:36 -0700

    rewriting routes and templates

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ app = Flask(__name__)
 load_dotenv()
 client = MongoClient('localhost', 27017)
 db = client.space
-space_items = db.space_items    #sets up space items as a database
-test_items = db.test_items
 
-test_items = [
+# sets up databases
+space_items = db.space_items    #sets up space items as a database
+cart_items = db.cart_items      #items for user to add to cart from space items
+devices = db.devices
+
+
+space_items = [
     {'title': 'Bubble Mask', 'description':'Vacuum Face Mask', 'price': '3000 sc', 'image': './static/bubble.jpg'},
     {'title': 'Towel', 'description':'For all needs', 'price': '42 sc'},
     {'title': 'Forcefield', 'description': 'Protect from outside intrusion', 'price': '5,000,000 sc'}
@@ -21,40 +25,41 @@ test_items = [
 
 # Main route
 @app.route('/')
-def test():
+def home():
     '''Main page setup function'''
-    return render_template('store_index.html', space_items=space_items.find())
+    return render_template('homepage.html', space_items=space_items)
 
 
 #Route for VIEWING store items on homepage
 @app.route('/store', methods=['GET'])
 def store_display():
-    return render_template('store_display.html', test_items=test_items)
+    return render_template('store_display.html', space_items=space_items)
 
 
-# Route for VIEWING to cart
-@app.route('/store/cart', methods=['GET'])
+# ~~~~~~~~  CART  ~~~~~~~~
+
+# VIEWING cart homepage
+@app.route('/cart', methods=['GET'])
 def cart_display():
-    return render_template('cart_display.html') 
+    return render_template('cart_display.html')
 
-# Route for ADDING to cart
-@app.route('/store/cart', methods=['POST'])
-def cart_submit():
-    '''Add items to cart'''
-    space_item = {
-    'title': request.form.get('title'),
-    'description': request.form.get('description'),
-    'price': request.form.get('price'),
-    'image': request.form.get('image'.split())
+# ADD items to cart
+@app.route('/cart/add')
+def devices_new():
+    '''Create devices'''
+    return render_template('cart_add.html')
+
+# Route for POSTING items to cart
+@app.route('/cart', methods=['POST'])
+def cart_post():
+    '''Submit items to cart'''
+    cart= {
+        'title': request.form.get('title'),
+        'description': request.form.get('description')
     }
-    space_item_id = space_items.insert_one(space_item).inserted_id
-    return redirect(url_for('cart_items_show', space_item_id=space_item_id))
+    cart_items.insert_one(cart)
+    return redirect(url_for('cart_display')) 
 
-#Show cart items
-@app.route('/store/cart/<space_items_id>')
-def cart_show(space_item_id):
-    space_item = space_items.find_one({'_id': ObjectId(space_item_id)})
-    return render_template('cart_display.html', space_item=space_item)
 
 
 if __name__=='__main__':
